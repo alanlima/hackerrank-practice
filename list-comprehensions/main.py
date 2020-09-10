e7f38b3c1086782a8da9c9aa62ba02246b30252d
Alan Lima <alima@outlook.com>
2020-09-10 11:14:04 +1000

    solve problem: list-comprehensions

---
+++ list-comprehensions/main.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    result = [ [ xx, yy, zz ] 
+        for xx in range(0, x + 1) 
+        for yy in range(0, y + 1) 
+        for zz in range(0, z + 1) 
+        if xx + yy + zz != n ]
+ 
+    print(result)
